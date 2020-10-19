d3b3c2cbe41f308aee2b32aeb8c3579920f0fe71
Quentin-Gérome OMEL <quentin.bartschs@gmail.com>
2020-10-19 11:22:35 +0200

    AI
    Set up for FANN

--- Assets/AI/TP3 - Fann/Scripts/Fann.cs
+++ Assets/AI/TP3 - Fann/Scripts/Fann.cs
@@ -1,10 +1,16 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
+using FANNCSharp.Double;
+using System.Collections.Generic;
 
 namespace AI_FANN
 {
     public class Fann : MonoBehaviour
     {
-
+        public void FANN()
+        {
+            List<uint> layers = new List<uint>();
+            NeuralNet network = new NeuralNet(FANNCSharp.NetworkType.LAYER, layers);
+        }
+        
     }
 }
