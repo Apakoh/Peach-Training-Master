79b62db0772743081582f190a889d0d6bcb9b9a4
Quentin-Gérome OMEL <quentin.bartschs@gmail.com>
2020-10-20 10:06:35 +0200

    Synthese
    Random Velocity + Adding Collider to points

--- Assets/Synthese/Scripts/GravitySimulator.cs
+++ Assets/Synthese/Scripts/GravitySimulator.cs
@@ -8,9 +8,16 @@ namespace Synthese
     {
         private List<Point> list_points = new List<Point>();
 
+        private const float gravity = 9.8f;
+
         private void Start()
         {
             this.list_points = this.GetComponent<PointFactory>().list_points;
+
+            foreach(Point pt in this.list_points)
+            {
+                RandomVelocity(pt);
+            }
         }
 
         private void Update()
@@ -21,6 +28,16 @@ namespace Synthese
         private void Gravity()
         {
 
+        }
+
+        private void RandomVelocity(Point point)
+        {
+            point.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(RandomVector3(5f), RandomVector3(5f), RandomVector3(5f));
+        }
+
+        private float RandomVector3(float range)
+        {
+            return Random.Range(-range, range);
         }
     }
 }
