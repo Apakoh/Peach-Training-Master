0de6f698f2a541a35027ca19c719c949eaab72bd
Quentin-Gérome OMEL <quentin.bartschs@gmail.com>
2020-10-20 09:59:39 +0200

    Synthese
    Adding Gravity SImulator (Set up)

---
+++ Assets/Synthese/Scripts/GravitySimulator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Synthese
+{
+    public class GravitySimulator : MonoBehaviour
+    {
+        private List<Point> list_points = new List<Point>();
+
+        private void Start()
+        {
+            this.list_points = this.GetComponent<PointFactory>().list_points;
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        private void Gravity()
+        {
+
+        }
+    }
+}
